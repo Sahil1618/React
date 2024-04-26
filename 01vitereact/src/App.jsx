766977ce96af9f30c7333e84ccbe1cad9993aaca
Sahil1618 <ss8876584@gmail.com>
2024-04-26 12:25:54 +0530

    counter in react and usestate

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,12 @@
 import Chai from "./chai"
 
 function App() {
-  
+  const username = "chai aur code"
 
   return (
     <>
       <Chai/>
-      <h1>Hello</h1>
+      <h1>Hello {username}</h1>
     </>
     
   )
